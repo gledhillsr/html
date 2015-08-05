361620a0a166e3b04d8b5fb789cbce3104237e94
Steve Gledhill <gledhillsr@familysearch.org>
2015-08-04 22:01:45 -0600

    update all index.html to fully qualify starting servlet

--- AlpineMt/index.php
+++ AlpineMt/index.php
@@ -1,45 +1,46 @@
 <?php
-  require("config.php");
-  if($ID) {
-    $szID1 = "?ID=$ID";
-    $szID2 = "&ID=$ID";
-  } else {
-    $szID1 ="";
-    $szID2 ="";
-  }
+require("config.php");
+if (isset($ID)) {
+  $szID1 = "?ID=$ID";
+  $szID2 = "&ID=$ID";
+} else {
+  $szID1 = "";
+  $szID2 = "";
+}
 
-  if($NSPgoto)
-    $firstPage=$NSPgoto;
-  else
-    $firstPage="MonthCalendar";
+if (isset($NSPgoto)) {
+  $firstPage = $NSPgoto;
+} else {
+  $firstPage = "MonthCalendar";
+}
 
 ?>
 <html>
 
 <head>
-<SCRIPT Language="JavaScript">
-<!--
-if (self != top)
-    top.location = self.location;
-//-->
-</SCRIPT>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
-<META HTTP-EQUIV="Expires" CONTENT="-1">
-<title><?php echo $resort; ?> NSP</title>
+  <SCRIPT Language="JavaScript">
+    <!--
+    if (self != top)
+      top.location = self.location;
+    //-->
+  </SCRIPT>
+  <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+  <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+  <META HTTP-EQUIV="Expires" CONTENT="-1">
+  <title><?php echo $resort; ?> NSP</title>
 
 </head>
 
 <frameset rows="113,*">
-<?php
+  <?php
   echo "<frame name=\"banner\" scrolling=\"no\" noresize target=\"contents\" src=\"resort_header.php$szID1\">";
   echo "<frame name=\"main\" src=\"http://nsponline.org:8080/calendar-1/$firstPage?resort=$resort{$szID2}&noLogin=1\" target=\"_self\" scrolling=\"auto\">";
-?>
+  ?>
   <noframes>
-  <body>
+    <body>
 
-  <p>This page uses frames, but your browser doesn't support them.
-  </body>
+    <p>This page uses frames, but your browser doesn't support them.
+    </body>
   </noframes>
-  </frameset>
+</frameset>
 </html>
