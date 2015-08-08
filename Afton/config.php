82d3005a4ade164bbcdddefff246245cb8db2318
Steve Gledhill <gledhillsr@familysearch.org>
2015-08-08 17:00:30 -0600

    added resorts upto IFNordic (except Brighton)

--- Afton/config.php
+++ Afton/config.php
@@ -5,26 +5,6 @@
 $resortFull = "Afton Alps";
 $resort     = "Afton";
 $resortURL  = "http://www.aftonalpsskipatrol.org";
-$resortImg  = "AftonLogo.jpg";
+$resortImg  = "/images/AftonLogo.jpg";
 $imgHeight  = 80;
-
-/*****************************************************************************
- * register cookies, POST's, & GET's                                         *
- *****************************************************************************/
-
-//if(!ini_get('register_globals'))
-{
-	$__am = array('COOKIE','POST','GET');
-	while(list(,$__m) = each($__am)){
-//var_dump($__m);
-//echo "---" . ${"HTTP_".$__m."_VARS"} . "---<br>";
-		$__ah = &${"HTTP_".$__m."_VARS"};
-//var_dump($__ah);
-		if(!is_array($__ah)) continue;
-		while(list($__n, $__v) = each ($__ah)) {
-//echo "yyy(".$__n."--".$__v.")<br>";
-			$$__n = $__v;
-		}
-	}
-}
 ?>
--- AlpineMt/config.php
+++ AlpineMt/config.php
@@ -5,26 +5,6 @@
 $resortFull = "Alpine Mt";
 $resort     = "AlpineMt";
 $resortURL  = "http://www.alpinemtskipatrol.org/";
-$resortImg  = "AlpineMt.jpg";
+$resortImg  = "/images/AlpineMt.jpg";
 $imgHeight  = 80;
-
-/*****************************************************************************
- * register cookies, POST's, & GET's                                         *
- *****************************************************************************/
-
-//if(!ini_get('register_globals'))
-{
-	$__am = array('COOKIE','POST','GET');
-	while(list(,$__m) = each($__am)){
-//var_dump($__m);
-//echo "---" . ${"HTTP_".$__m."_VARS"} . "---<br>";
-		$__ah = &${"HTTP_".$__m."_VARS"};
-//var_dump($__ah);
-		if(!is_array($__ah)) continue;
-		while(list($__n, $__v) = each ($__ah)) {
-//echo "yyy(".$__n."--".$__v.")<br>";
-			$$__n = $__v;
-		}
-	}
-}
 ?>
