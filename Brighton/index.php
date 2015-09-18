2ae8e737b58314588bc1e359eb22b4e587dd35d1
Steve Gledhill <gledhillsr@familysearch.org>
2015-09-18 11:02:03 -0600

    moved a-g resorts to use new index.php

--- Brighton/index.php
+++ Brighton/index.php
@@ -1,48 +1,19 @@
 <?php
-
-  require("config.php");
-
-  if($ID) {
-    $szID1 = "?ID=$ID";
-    $szID2 = "&ID=$ID";
-  } else {
-    $szID1 ="";
-    $szID2 ="";
-  }
-
-  if($NSPgoto)
-    $firstPage=$NSPgoto;
-  else
-    $firstPage="MonthCalendar";
-
-$resort="Brighton";
+$thisResort = "Brighton";
 ?>
-<html>
 
+<!DOCTYPE HTML>
+<html lang="en-US">
 <head>
-<SCRIPT Language="JavaScript">
-<!--
-if (self != top)
-    top.location = self.location;
-//-->
-</SCRIPT>
-<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
-<META HTTP-EQUIV="Expires" CONTENT="-1">
-<title><?php echo $resort; ?> NSP</title>
-
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="1;url=/calendar-1/MonthCalendar?resort=<?php echo $thisResort?>">
+  <script type="text/javascript">
+    window.location.href = "/calendar-1/MonthCalendar?resort=<?php echo $thisResort?>"
+  </script>
+  <title>Page Redirection</title>
 </head>
-
-<frameset rows="125,*">
-<?php
-  echo "<frame name=\"banner\" scrolling=\"no\" target=\"contents\" src=\"resort_header.php$szID1\">";
-  echo "<frame name=\"main\" src=\"http://nsponline.org:8080/calendar-1/$firstPage?resort=$resort{$szID2}&noLogin=1\" target=\"_self\" scrolling=\"auto\">";
-?>
-  <noframes>
-  <body>
-
-  <p>This page uses frames, but your browser doesn't support them.
-  </body>
-  </noframes>
-  </frameset>
+<body>
+<!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+If you are not redirected automatically, follow the <a href='/calendar-1/MonthCalendar?resort=<?php echo $thisResort?>'>link to <?php echo $thisResort?> Resort</a>
+</body>
 </html>
--- Brighton/jsp/security/error.txt
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>Login Error</title>
-</head>
-<body bgcolor=white>
-Login error -- please try <a href="login.jsp">again</a>.
-</body>
-</html>
