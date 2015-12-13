3f690071470fb8045dd5bdf929ab15b02c3757c9
Steve Gledhill <gledhillsr@familysearch.org>
2015-12-13 11:20:47 -0700

    hopefully fixed broken new patroller submit bug

---
+++ Brighton/VolunteerSubmit.php
@@ -0,0 +1,17 @@
+<head>
+  <meta http-equiv=Content-Type content="text/html; charset=windows-1252">
+
+  <title>Volunteer at Brighton</title>
+
+</head>
+
+<body lang=EN-US link=blue vlink=purple style='tab-interval:.5in' BACKGROUND="/images/ncmnthbk.jpg">
+<h1>Your application was successfully submitted.</h1>
+
+<?php
+$firstName = $_REQUEST['firstName'];
+$lastName = $_REQUEST['lastName'];
+echo "Thank you $firstName $lastName";
+?>
+
+</body>
