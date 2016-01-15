e9166fd6b552ab71be8e68f0b24411d876bc4b5b
Steve Gledhill <gledhillsr@familysearch.org>
2016-01-14 20:13:02 -0700

    initial add of ParkCidy

--- newResort/NewResort.sql
+++ newResort/NewResort.sql
@@ -1,139 +1,197 @@
-# phpMyAdmin SQL Dump
-# version 2.5.3
-# http://www.phpmyadmin.net
-#
-# Host: localhost
-# Generation Time: Jan 30, 2007 at 01:34 PM
-# Server version: 4.0.18
-# PHP Version: 4.3.5
-#
-# Database : `RMSP`
-#
+-- phpMyAdmin SQL Dump
+-- version 4.4.11
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 11, 2016 at 12:56 AM
+-- Server version: 5.5.46
+-- PHP Version: 5.6.14
 
-# --------------------------------------------------------
-
-#
-# Table structure for table `assignments`
-#
-
-CREATE TABLE `assignments` (
-  `Date` varchar(14) NOT NULL default '',
-  `StartTime` varchar(12) default NULL,
-  `EndTime` varchar(12) default NULL,
-  `EventName` varchar(128) default NULL,
-  `ShiftType` tinyint(4) NOT NULL default '0',
-  `Count` tinyint(4) NOT NULL default '0',
-  `P0` varchar(7) default NULL,
-  `P1` varchar(7) default NULL,
-  `P2` varchar(7) default NULL,
-  `P3` varchar(7) default NULL,
-  `P4` varchar(7) default NULL,
-  `P5` varchar(7) default NULL,
-  `P6` varchar(7) default NULL,
-  `P7` varchar(7) default NULL,
-  `P8` varchar(7) default NULL,
-  `P9` varchar(7) default NULL,
-  PRIMARY KEY  (`Date`)
-) TYPE=MyISAM;
-
-#
-# Dumping data for table `assignments`
-#
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-# --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-#
-# Table structure for table `directorsettings`
-#
+--
+-- Database: `WelchVillage`
+--
+CREATE DATABASE IF NOT EXISTS `WelchVillage` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `WelchVillage`;
 
-CREATE TABLE `directorsettings` (
-  `PatrolName` varchar(32) NOT NULL default '',
-  `emailReminder` char(1) NOT NULL default '0',
-  `reminderDays` tinyint(4) NOT NULL default '3',
-  `emailOnChanges` char(1) NOT NULL default '0',
-  `useTeams` char(1) NOT NULL default '0',
-  `directorsOnlyChange` char(1) NOT NULL default '0',
-  `emailAll` char(1) NOT NULL default '0',
-  `nameFormat` tinyint(4) NOT NULL default '0',
-  `startDate` varchar(6) default NULL,
-  `endDate` varchar(6) default NULL,
-  `useBlackOut` tinyint(4) NOT NULL default '0',
-  `startBlackOut` varchar(8) NOT NULL default '01-02-03',
-  `endBlackOut` varchar(8) NOT NULL default '02-03-03',
-  `lastSkiHistoryUpdate` int(11) NOT NULL default '0',
-  `lastVoucherHistoryUpdate` date NOT NULL default '0000-00-00',
-  `signinLockout` int(11) NOT NULL default '0',
-  `removeAccess` tinyint(4) NOT NULL default '0',
-  PRIMARY KEY  (`PatrolName`)
-) TYPE=MyISAM;
+-- --------------------------------------------------------
 
-#
-# Dumping data for table `directorsettings`
-#
+--
+-- Table structure for table `assignments`
+--
 
-INSERT INTO `directorsettings` VALUES ('Ragged Mountain', '1', 2, '1', '0', '0', '0', 0, '23-11', '05-04', 0, '01-02-05', '03-04-05', 0, '0000-00-00', 0, 0);
-
-# --------------------------------------------------------
-
-#
-# Table structure for table `roster`
-#
-
-CREATE TABLE `roster` (
-  `IDNumber` varchar(6) NOT NULL default '',
-  `ClassificationCode` varchar(4) default NULL,
-  `LastName` varchar(24) NOT NULL default '',
-  `FirstName` varchar(24) NOT NULL default '',
-  `Spouse` varchar(24) default NULL,
-  `Address` varchar(48) default NULL,
-  `City` varchar(32) default NULL,
-  `State` varchar(16) default NULL,
-  `ZipCode` varchar(10) default NULL,
-  `HomePhone` varchar(26) default NULL,
-  `WorkPhone` varchar(26) default NULL,
-  `CellPhone` varchar(26) default NULL,
-  `Pager` varchar(26) default NULL,
-  `email` varchar(48) default NULL,
-  `EmergencyCallUp` varchar(8) default NULL,
-  `Password` varchar(16) NOT NULL default '',
-  `NightSubsitute` varchar(4) default NULL,
-  `Commitment` tinyint(4) NOT NULL default '2',
-  `Instructor` tinyint(4) NOT NULL default '0',
-  `Director` varchar(10) default NULL,
-  `lastUpdated` date NOT NULL default '2003-01-01',
-  `carryOverCredits` smallint(6) NOT NULL default '0',
-  `lastCreditUpdate` bigint(11) NOT NULL default '0',
-  `canEarnCredits` tinyint(4) NOT NULL default '0',
-  `creditsEarned` smallint(6) NOT NULL default '0',
-  `creditsUsed` smallint(6) NOT NULL default '0',
-  `teamLead` tinyint(4) NOT NULL default '0',
-  `mentoring` tinyint(4) NOT NULL default '0',
-  `comments` text NOT NULL,
-  PRIMARY KEY  (`IDNumber`)
-) TYPE=MyISAM;
-
-#
-# Dumping data for table `roster`
-#
+DROP TABLE IF EXISTS `assignments`;
+CREATE TABLE IF NOT EXISTS `assignments` (
+  `Date` varchar(14) NOT NULL DEFAULT '',
+  `StartTime` varchar(12) DEFAULT NULL,
+  `EndTime` varchar(12) DEFAULT NULL,
+  `EventName` varchar(128) DEFAULT NULL,
+  `ShiftType` tinyint(4) NOT NULL DEFAULT '0',
+  `Count` tinyint(4) NOT NULL DEFAULT '0',
+  `P0` varchar(7) DEFAULT NULL,
+  `P1` varchar(7) DEFAULT NULL,
+  `P2` varchar(7) DEFAULT NULL,
+  `P3` varchar(7) DEFAULT NULL,
+  `P4` varchar(7) DEFAULT NULL,
+  `P5` varchar(7) DEFAULT NULL,
+  `P6` varchar(7) DEFAULT NULL,
+  `P7` varchar(7) DEFAULT NULL,
+  `P8` varchar(7) DEFAULT NULL,
+  `P9` varchar(7) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
-# --------------------------------------------------------
+-- --------------------------------------------------------
 
-#
-# Table structure for table `shiftdefinitions`
-#
+--
+-- Table structure for table `directorsettings`
+--
 
-CREATE TABLE `shiftdefinitions` (
-  `EventName` varchar(128) NOT NULL default '',
-  `StartTime` varchar(16) NOT NULL default '',
-  `EndTime` varchar(16) NOT NULL default '',
-  `Count` tinyint(4) NOT NULL default '0',
-  `ShiftType` tinyint(4) NOT NULL default '0',
-  PRIMARY KEY  (`EventName`)
-) TYPE=MyISAM;
+DROP TABLE IF EXISTS `directorsettings`;
+CREATE TABLE IF NOT EXISTS `directorsettings` (
+  `PatrolName` varchar(32) NOT NULL DEFAULT '',
+  `emailReminder` char(1) NOT NULL DEFAULT '0',
+  `reminderDays` tinyint(4) NOT NULL DEFAULT '3',
+  `emailOnChanges` char(1) NOT NULL DEFAULT '0',
+  `useTeams` char(1) NOT NULL DEFAULT '0',
+  `directorsOnlyChange` char(1) NOT NULL DEFAULT '0',
+  `emailAll` char(1) NOT NULL DEFAULT '0',
+  `nameFormat` tinyint(4) NOT NULL DEFAULT '0',
+  `startDate` varchar(6) DEFAULT NULL,
+  `endDate` varchar(6) DEFAULT NULL,
+  `useBlackOut` tinyint(4) NOT NULL DEFAULT '0',
+  `startBlackOut` varchar(8) NOT NULL DEFAULT '01-02-03',
+  `endBlackOut` varchar(8) NOT NULL DEFAULT '02-03-03',
+  `lastSkiHistoryUpdate` int(11) NOT NULL DEFAULT '0',
+  `lastVoucherHistoryUpdate` date NOT NULL DEFAULT '0000-00-00',
+  `signinLockout` int(11) NOT NULL DEFAULT '0',
+  `removeAccess` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-#
-# Dumping data for table `shiftdefinitions`
-#
+--
+-- Dumping data for table `directorsettings`
+--
 
+INSERT INTO `directorsettings` (`PatrolName`, `emailReminder`, `reminderDays`, `emailOnChanges`, `useTeams`, `directorsOnlyChange`, `emailAll`, `nameFormat`, `startDate`, `endDate`, `useBlackOut`, `startBlackOut`, `endBlackOut`, `lastSkiHistoryUpdate`, `lastVoucherHistoryUpdate`, `signinLockout`, `removeAccess`) VALUES
+('Welch Village', '0', 3, '0', '0', '0', '1', 0, '13-11', '29-03', 0, '01-01-14', '01-01-14', 0, '0000-00-00', 0, 127);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newindividualassignment`
+--
+
+DROP TABLE IF EXISTS `newindividualassignment`;
+CREATE TABLE IF NOT EXISTS `newindividualassignment` (
+  `date_shift_pos` varchar(18) DEFAULT NULL,
+  `scheduledate` date DEFAULT NULL,
+  `shiftType` tinyint(4) NOT NULL,
+  `flags` smallint(6) NOT NULL,
+  `patrollerId` varchar(6) NOT NULL,
+  `lastModifiedDate` datetime DEFAULT NULL,
+  `lastModifiedBy` varchar(6) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roster`
+--
+
+DROP TABLE IF EXISTS `roster`;
+CREATE TABLE IF NOT EXISTS `roster` (
+  `IDNumber` varchar(6) NOT NULL DEFAULT '',
+  `ClassificationCode` varchar(4) DEFAULT NULL,
+  `LastName` varchar(24) NOT NULL DEFAULT '',
+  `FirstName` varchar(24) NOT NULL DEFAULT '',
+  `Spouse` varchar(24) DEFAULT '',
+  `Address` varchar(48) DEFAULT '',
+  `City` varchar(32) DEFAULT NULL,
+  `State` varchar(16) DEFAULT NULL,
+  `ZipCode` varchar(10) DEFAULT NULL,
+  `HomePhone` varchar(26) DEFAULT NULL,
+  `WorkPhone` varchar(26) DEFAULT '',
+  `CellPhone` varchar(26) DEFAULT '',
+  `Pager` varchar(26) DEFAULT '',
+  `email` varchar(48) DEFAULT '',
+  `EmergencyCallUp` varchar(8) DEFAULT '',
+  `Password` varchar(16) NOT NULL DEFAULT '',
+  `NightSubsitute` varchar(4) DEFAULT '',
+  `Commitment` tinyint(4) NOT NULL DEFAULT '2',
+  `Instructor` tinyint(4) NOT NULL DEFAULT '0',
+  `Director` varchar(10) DEFAULT 'no',
+  `lastUpdated` date NOT NULL DEFAULT '2003-01-01',
+  `carryOverCredits` smallint(6) NOT NULL DEFAULT '0',
+  `lastCreditUpdate` bigint(11) NOT NULL DEFAULT '0',
+  `canEarnCredits` tinyint(4) NOT NULL DEFAULT '0',
+  `creditsEarned` smallint(6) NOT NULL DEFAULT '0',
+  `creditsUsed` smallint(6) NOT NULL DEFAULT '0',
+  `teamLead` tinyint(4) NOT NULL DEFAULT '0',
+  `mentoring` tinyint(4) NOT NULL DEFAULT '0',
+  `comments` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shiftdefinitions`
+--
+
+DROP TABLE IF EXISTS `shiftdefinitions`;
+CREATE TABLE IF NOT EXISTS `shiftdefinitions` (
+  `EventName` varchar(128) NOT NULL DEFAULT '',
+  `StartTime` varchar(16) NOT NULL DEFAULT '',
+  `EndTime` varchar(16) NOT NULL DEFAULT '',
+  `Count` tinyint(4) NOT NULL DEFAULT '0',
+  `ShiftType` tinyint(4) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shiftdefinitions`
+--
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `assignments`
+--
+ALTER TABLE `assignments`
+  ADD PRIMARY KEY (`Date`);
+
+--
+-- Indexes for table `directorsettings`
+--
+ALTER TABLE `directorsettings`
+  ADD PRIMARY KEY (`PatrolName`);
+
+--
+-- Indexes for table `newindividualassignment`
+--
+ALTER TABLE `newindividualassignment`
+  ADD UNIQUE KEY `date_index_pos` (`date_shift_pos`);
+
+--
+-- Indexes for table `roster`
+--
+ALTER TABLE `roster`
+  ADD PRIMARY KEY (`IDNumber`);
+
+--
+-- Indexes for table `shiftdefinitions`
+--
+ALTER TABLE `shiftdefinitions`
+  ADD PRIMARY KEY (`EventName`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
