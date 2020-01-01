2f778837c80262726c637d37f0cf8144d37b505c
Steve Gledhill <gledhillsr@familysearch.org>
2020-01-01 16:50:00 -0700

    add psiaPimeMountain

---
+++ psiaPineMountain/index.php
@@ -0,0 +1,19 @@
+<?php
+$thisResort = "psiaPineMountain";
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="1;url=/calendar-1/MonthCalendar?resort=<?php echo $thisResort?>">
+  <script type="text/javascript">
+    window.location.href = "/calendar-1/MonthCalendar?resort=<?php echo $thisResort?>"
+  </script>
+  <title>Page Redirection</title>
+</head>
+<body>
+<!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+If you are not redirected automatically, follow the <a href='/calendar-1/MonthCalendar?resort=<?php echo $thisResort?>'>link to <?php echo $thisResort?> Resort</a>
+</body>
+</html>
