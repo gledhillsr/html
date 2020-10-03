a7867984259d9d5e66219628e2921137673488c9
Steve Gledhill <gledhillsr@familysearch.org>
2020-10-03 13:55:33 -0600

    update 30

---
+++ Brighton/training/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Brighton Ski Patrol Training</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body background="img/background.jpg" bottommargin="0" leftmargin="0" marginheight="0" marginwidth="0" rightmargin="0" topmargin="0">
+
+	
+<center><table width="765" height="100%" cellpadding="0" cellspacing="0" border="0" background="img/mainbackground.jpg"><tr valign="top"><td>
+
+<table width="764" height="97" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+<td width="248"><!-- logo image --><img src="Brighton.gif" width="248" height="97" border="0" alt=""><!-- end logo image --></td>
+<td width="100%" background="img/toplogobg.jpg">
+	<font size="6" color="black"><center>Brighton Ski Patrol Training 2020/2021</center></font></td>
+	</tr>
+</table>
+<table width="764" height="42" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+<td width="169"><img src="img/left1.jpg" width="169" height="42" border="0" alt=""></td>
+<td width="100%" background="img/left1bg.jpg"><img src="img/left1bg.jpg" width="20" height="42" border="0" alt=""></td>
+	</tr>
+</table>
+<table width="764" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+
+<td width="150">
+<!-- menu below, copy and paste the menudivider.jpg code as necessary -->
+<!-- the &nbsp; is code for a one letter space -->
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showOverview()">Overview</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showLiftEvac()">Lift Evac</a><br>
+<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showPPE()">PPE</a><br>-->
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showCPR()">CPR</a><br>
+<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Naloxone</a><br>-->
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Aid Room Ops</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Cleaning Plans</a><br>
+<!--
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Avalance Safety</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Brighton Ski Patrol</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-National Ski Patrol</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Transportation</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Covid-19</a><br>
+-->
+<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Screen Area</a><br>-->
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showKnots()">Knots and Terms</a><br>
+<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Proficiency Tests</a><br>-->
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Contact Info</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showASL()">ASL</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>
+&nbsp;&nbsp;dash=Under construction
+<!-- end menu -->
+</td>
+<td width="10">&nbsp;</td>
+<td width="744">
+
+<!-- add your content below -->
+	<iframe id="panel" width="100%" height="600" style="border:none;" src="Overview.html"></iframe>
+	
+	<script>
+      function showOverview() {
+        document.getElementById("panel").src="Overview.html";
+      }
+      function showPPE() {
+        document.getElementById("panel").src="PPE.html";
+      }
+      function showLiftEvac() {
+        document.getElementById("panel").src="LiftEvac.html";
+      }
+      function showCPR() {
+        document.getElementById("panel").src="CPR.html";
+      }
+      function showWIP() {
+        document.getElementById("panel").src="UnderConstruction.html";
+      }
+      function showKnots() {
+        document.getElementById("panel").src="KnotsAndTerms.pdf";
+      }
+      function showASL() {
+        document.getElementById("panel").src="brighton_asl.pdf";
+      }
+		
+//	  <document.getElementById("myFrame").onload = function() {showPPE()};
+    </script>
+	
+<br>
+<center>Web site and all contents © Copyright Brighton Ski Patrol, All rights reserved.</center>
+<!-- end content area -->
+
+
+<center><a href="http://www.steves-templates.com">Free website templates</a></center>
+</td>
+<td width="10">&nbsp;</td>
+	</tr>
+</table>
+</td></tr></table></center>
+</body>
+</html>
