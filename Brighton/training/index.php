083f4a8e6d8e1280b927007e20f23bdc5e88c915
Steve Gledhill <gledhillsr@familysearch.org>
2020-10-04 12:22:13 -0600

    update32

--- Brighton/training/index.php
+++ Brighton/training/index.php
@@ -1,5 +1,5 @@
 <?php 
-require("config.php");
+ require("config.php");
  $connect_string = @mysql_connect($mysql_host, $mysql_username, $mysql_password) or die ("Could not connect to the database.");
  $strToday = "xyz";   
 ?>
@@ -9,8 +9,7 @@ require("config.php");
 <head>
 	<title>Brighton Ski Patrol Training</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-<!--suppress JSUnresolvedLibraryURL -->
-  <script src="http://code.jquery.com/jquery-1.9.1.js"></script></head>
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script></head>
 
 <body background="img/background.jpg" bottommargin="0" leftmargin="0" marginheight="0" marginwidth="0" rightmargin="0" topmargin="0">
 
@@ -19,42 +18,28 @@ require("config.php");
 
 <table width="764" height="97" cellpadding="0" cellspacing="0" border="0">
 	<tr valign="top">
-<td width="248"><!-- logo image --><img src="Brighton.gif" width="248" height="97" border="0" alt=""><!-- end logo image --></td>
-<td width="100%" background="img/toplogobg.jpg">
+	<td width="248"><!-- logo image --><img src="Brighton.gif" width="248" height="97" border="0" alt=""><!-- end logo image --></td>
+	<td width="100%" background="img/toplogobg.jpg">
 	<font size="6" color="black"><center>Brighton Ski Patrol Training 2020/2021</center></font></td>
 	</tr>
 </table>
 <table width="764" height="42" cellpadding="0" cellspacing="0" border="0">
 	<tr valign="top">
-<td width="169"><img src="img/left1.jpg" width="169" height="42" border="0" alt=""></td>
-<td width="100%" background="img/left1bg.jpg"><img src="img/left1bg.jpg" width="20" height="42" border="0" alt=""></td>
+	<td width="169"><img src="img/left1.jpg" width="169" height="42" border="0" alt=""></td>
+	<td width="100%" background="img/left1bg.jpg"><img src="img/left1bg.jpg" width="20" height="42" border="0" alt=""></td>
 	</tr>
 </table>
 <table width="764" cellpadding="0" cellspacing="0" border="0">
 	<tr valign="top">
 
 <td width="150">
-<!-- menu below, copy and paste the menudivider.jpg code as necessary -->
-<!-- the &nbsp; is code for a one letter space -->
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showOverview()">Overview</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showLiftEvac()">Lift Evac</a><br>
-<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showPPE()">PPE</a><br>-->
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showCPR()">CPR</a><br>
-<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Naloxone</a><br>-->
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">Thing 1</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">Thing 2</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">Thing 3</a><br>
-<!--
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Avalance Safety</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Brighton Ski Patrol</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-National Ski Patrol</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Transportation</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Covid-19</a><br>
--->
-<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Screen Area</a><br>-->
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showKnots()">Knots and Terms</a><br>
-<!--<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showWIP()">-Proficiency Tests</a><br>-->
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showASL()">ASL</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>
 &nbsp;&nbsp;
 
@@ -72,47 +57,43 @@ require("config.php");
 	echo "<br><br><br>&nbsp;Welcome<br><b>&nbsp;$firstName $lastName</b><br><br>&nbsp;$strToday";
 ?>
 	
-<!-- end menu -->
 </td>
 <td width="10">&nbsp;</td>
 <td width="744">
 
-<!-- add your content below -->
 	<iframe id="panel" width="100%" height="600" style="border:none;" src="Overview.html"></iframe>
 	
 	<script>
-      function showOverview() {
-        document.getElementById("panel").src="Overview.html";
-      }
-      function showPPE() {
-		log("PPE");  
-        document.getElementById("panel").src="PPE.html";
-      }
-	  function log($menuItem) {
-		global $firstName, $lastName, $strToday, $id;  
-		error_log("zz $menuItem page ($strToday) ~ $id $firstName $lastName");
-	  }			
-      function showLiftEvac() {
-		log("LiftEvac");  
-        document.getElementById("panel").src="LiftEvac.html";
-      }
-      function showCPR() {
-		log("CPR");  
-        document.getElementById("panel").src="CPR.html";
-      }
-      function showWIP() {
-        document.getElementById("panel").src="UnderConstruction.html";
-      }
-      function showKnots() {
-		log("Knots");  
-        document.getElementById("panel").src="KnotsAndTerms.pdf";
-      }
-      function showASL() {
-		log("ASL");  
-        document.getElementById("panel").src="brighton_asl.pdf";
-      }
-		
-//	  <document.getElementById("myFrame").onload = function() {showPPE()};
+	function showOverview() {
+		document.getElementById("panel").src="Overview.html";
+	}
+	function showPPE() {
+//		log("PPE");  
+		document.getElementById("panel").src="PPE.html";
+	}
+//	function log($menuItem) {
+//		global $firstName, $lastName, $strToday, $id;  
+//		error_log("zz $menuItem page ($strToday) ~ $id $firstName $lastName");
+//	}			
+	function showLiftEvac() {
+//		log("LiftEvac");  
+		document.getElementById("panel").src="LiftEvac.html";
+	}
+	function showCPR() {
+//		log("CPR");  
+		document.getElementById("panel").src="CPR.html";
+	}
+	function showWIP() {
+		document.getElementById("panel").src="UnderConstruction.html";
+	}
+	function showKnots() {
+//		log("Knots");  
+		document.getElementById("panel").src="KnotsAndTerms.pdf";
+	}
+	function showASL() {
+//		log("ASL");  
+		document.getElementById("panel").src="brighton_asl.pdf";
+	}		
     </script>
 	
 <br>
