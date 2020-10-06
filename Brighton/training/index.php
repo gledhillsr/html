8cce3a39071e9df17de92e9049aeccf757022875
Steve Gledhill <gledhillsr@familysearch.org>
2020-10-05 18:23:19 -0600

    early Transport

--- Brighton/training/index.php
+++ Brighton/training/index.php
@@ -37,7 +37,7 @@
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showPandemicResponse()">Pandemic Response</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showPPE()">-PPE</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showIDCProcedures()">IDC Prodecures</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showEarlyTransport()">-Early Transport</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showEarlyTransport()">Transport Decisions</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showScreeningArea()">-Screen Area Guide</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSymptomsC19()">Symptoms of Covid-19</a><br>
 <img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSymptomsC19a()">Symptoms of C19 dup</a><br>
@@ -91,7 +91,7 @@
 		document.getElementById("panel").src="IDC.html";
 	}
 	function showEarlyTransport() {
-		document.getElementById("panel").src="9-Early_Transport_Decisions-jc.pdf";
+		document.getElementById("panel").src="Transport.html";
 	}
 	function showScreeningArea() {
 		document.getElementById("panel").src="screening.html";
