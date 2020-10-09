6fb70e51026e6ee9174c0d14e47fff9d76d58060
Steve Gledhill <gledhillsr@familysearch.org>
2020-10-09 10:33:27 -0600

    lint

--- Brighton/training/index.php
+++ Brighton/training/index.php
@@ -8,75 +8,12 @@
 <html>
 <head>
 	<title>Brighton Ski Patrol Training</title>
+	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="http://code.jquery.com/jquery-1.9.1.js"></script></head>
 
 <body background="img/background.jpg" bottommargin="0" leftmargin="0" marginheight="0" marginwidth="0" rightmargin="0" topmargin="0">
-
-	
-<center><table width="765" height="100%" cellpadding="0" cellspacing="0" border="0" background="img/mainbackground.jpg"><tr valign="top"><td>
-
-<table width="764" height="97" cellpadding="0" cellspacing="0" border="0">
-	<tr valign="top">
-	<td width="248"><!-- logo image --><img src="Brighton.gif" width="248" height="97" border="0" alt=""><!-- end logo image --></td>
-	<td width="100%" background="img/toplogobg.jpg">
-	<font size="6" color="black"><center>Brighton Ski Patrol Training 2020/2021</center></font></td>
-	</tr>
-</table>
-<table width="764" height="42" cellpadding="0" cellspacing="0" border="0">
-	<tr valign="top">
-	<td width="169"><img src="img/left1.jpg" width="169" height="42" border="0" alt=""></td>
-	<td width="100%" background="img/left1bg.jpg"><img src="img/left1bg.jpg" width="20" height="42" border="0" alt=""></td>
-	</tr>
-</table>
-<table width="764" cellpadding="0" cellspacing="0" border="0">
-	<tr valign="top">
-
-<td width="150">
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showIntro()">2020 Introduction</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showBSPResponsePlan()">BSP Response Plan</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showPPE()">PPE</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showIDCProcedures()">IDC Prodecures</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showEarlyTransport()">Transport Decisions</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showScreeningArea()">Screening Area Guide</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSymptomsC19()">Symptoms of Covid-19</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSymptomsC19a()">Symptoms from CDC</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showNSAA()">NSAA Webinar</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showCPR()">CPR</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showNSPResponsePlan()">NSP Response Plan</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showStress()">Signs of Stress</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSnowSafety()">Snow Safety</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showNaloxone()">Naloxone</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showBSPDocumentation()">BSP Documentation</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showTriage()">Triage</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showLiftEvac()">Lift Evac</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showAssessment()">Patient Assessment</a><br>
-<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>
-&nbsp;&nbsp;
-
-<?php
-	$arrDate = getdate();
-	$today=mktime($arrDate['hours'], $arrDate['minutes'], 0, $arrDate['mon'], $arrDate['mday'], $arrDate['year']);
-	$strToday = date("m/d/Y g:i a", $today);
-    $query_string = "SELECT * FROM roster WHERE IDNumber=$id";
-	$result = @mysql_db_query($mysql_db, $query_string) or die ("Invalid query member id [$id] not recognized");
-	if ($row = @mysql_fetch_array($result)) {
-		$firstName = $row["FirstName"];
-		$lastName = $row["LastName"];
-	}
-
-	function mylog($menuItem) {
-	global $firstName, $lastName, $strToday, $id;  
-	error_log("zz $menuItem page ($strToday) ~ $id $firstName $lastName");
-	}			
-
-	mylog("main");
-	echo "<br><br><br>&nbsp;Welcome<br><b>&nbsp;$firstName $lastName</b><br><br>&nbsp;$strToday";
-?>
-	
-</td> 
-<td width="10">&nbsp;</td>
-<td width="744"><iframe id="panel" width="100%" height="600" style="border:none;" src="Overview.html"></iframe>  <script>
+<script>
 	function showIntro() {
 		document.getElementById("panel").src="Overview.html";
 	}
@@ -145,6 +82,70 @@
 		document.getElementById("panel").src="brighton_asl.pdf";
 	}		
     </script>
+	
+<center><table width="765" height="100%" cellpadding="0" cellspacing="0" border="0" background="img/mainbackground.jpg"><tr valign="top"><td>
+
+<table width="764" height="97" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+	<td width="248"><!-- logo image --><img src="Brighton.gif" width="248" height="97" border="0" alt=""><!-- end logo image --></td>
+	<td width="100%" background="img/toplogobg.jpg">
+	<font size="6" color="black"><center>Brighton Ski Patrol Training 2020/2021</center></font></td>
+	</tr>
+</table>
+<table width="764" height="42" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+	<td width="169"><img src="img/left1.jpg" width="169" height="42" border="0" alt=""></td>
+	<td width="100%" background="img/left1bg.jpg"><img src="img/left1bg.jpg" width="20" height="42" border="0" alt=""></td>
+	</tr>
+</table>
+<table width="764" cellpadding="0" cellspacing="0" border="0">
+	<tr valign="top">
+
+<td width="150">
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showIntro()">2020 Introduction</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showBSPResponsePlan()">BSP Response Plan</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showPPE()">PPE</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showIDCProcedures()">IDC Prodecures</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showEarlyTransport()">Transport Decisions</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showScreeningArea()">Screening Area Guide</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSymptomsC19()">Symptoms of Covid-19</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSymptomsC19a()">Symptoms from CDC</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showNSAA()">NSAA Webinar</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showCPR()">CPR</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showNSPResponsePlan()">NSP Response Plan</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showStress()">Signs of Stress</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showSnowSafety()">Snow Safety</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showNaloxone()">Naloxone</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showBSPDocumentation()">BSP Documentation</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showTriage()">Triage</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showLiftEvac()">Lift Evac</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>&nbsp; <a onclick="showAssessment()">Patient Assessment</a><br>
+<img src="img/menudivider.jpg" width="150" height="6" border="0" alt=""><br>
+&nbsp;&nbsp;
+
+<?php
+	$arrDate = getdate();
+	$today=mktime($arrDate['hours'], $arrDate['minutes'], 0, $arrDate['mon'], $arrDate['mday'], $arrDate['year']);
+	$strToday = date("m/d/Y g:i a", $today);
+    $query_string = "SELECT * FROM roster WHERE IDNumber=$id";
+	$result = @mysql_db_query($mysql_db, $query_string) or die ("Invalid query member id [$id] not recognized");
+	if ($row = @mysql_fetch_array($result)) {
+		$firstName = $row["FirstName"];
+		$lastName = $row["LastName"];
+	}
+
+	function mylog($menuItem) {
+	global $firstName, $lastName, $strToday, $id;  
+	error_log("zz $menuItem page ($strToday) ~ $id $firstName $lastName");
+	}			
+
+	mylog("main");
+	echo "<br><br><br>&nbsp;Welcome<br><b>&nbsp;$firstName $lastName</b><br><br>&nbsp;$strToday";
+?>
+	
+</td> 
+<td width="10">&nbsp;</td>
+<td width="744"><iframe id="panel" width="100%" height="600" style="border:none;" src="Overview.html"></iframe>  
 
 <br>
 <center>Web site and all contents (C) Copyright Brighton Ski Patrol, All rights reserved.</center>
